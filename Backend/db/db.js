e5ed39642b908fc0f01adff0726da4a35f76b9e2
Prayush <dpratyush412@gmail.com>
2024-07-27 21:36:07 +0530

    initial commit

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const db = async () => {
+    console.log('MONGO_URL:', process.env.MONGO_URL); // Debug log
     try {
         await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
