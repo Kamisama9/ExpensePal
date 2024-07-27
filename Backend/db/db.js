0ff1695c489fa8530d37fd3a356b6ac803de1510
Prayush <dpratyush412@gmail.com>
2024-07-27 21:24:58 +0530

    initial commit

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,13 +1,18 @@
-const mongoose=require('mongoose')
-require("dotenv").config();
-const db=async()=>{
-    mongoose.connect(process.env.MONGO_URL)
-    .then(() => console.log("DB CONNECTION SUCCESS"))
-    .catch((err) => {
-        console.log(`DB CONNECTION ISSUES`);
-        console.error(err.message);
-        process.exit(1);
-    });
-}
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-module.exports={db}
+const db = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('DB connected');
+    } catch (error) {
+        console.log('DB CONNECTION ISSUES');
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+module.exports = { db };
