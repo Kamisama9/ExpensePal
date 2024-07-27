6d24a7aae5dfaf3bff437fb29a94a6559e60a277
Prayush <dpratyush412@gmail.com>
2024-07-27 21:28:52 +0530

    initial commit

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const db = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
+        await mongoose.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
